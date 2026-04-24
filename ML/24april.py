f6d792e3c7016dbafe467aa9c268da29f97ff6e3
dishant1123 <dishantshah1417@gmail.com>
2026-04-24 17:23:23 +0530

    ....

--- ML/24april.py
+++ ML/24april.py
@@ -1,7 +1,7 @@
 """
 # confusion  matrix :                     predicted positive     predicted negative
-actual positive           TP                     FN
-actual negative           FP                     TN
+actual positive                             TP                     FN
+actual negative                             FP                     TN
 
 spam 
 not spam 
@@ -42,11 +42,11 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix,precision_score,recall_score,f1_score
 from sklearn.metrics import accuracy_score
 
 # step :1 
-data = {
+"""data = {
     'Age': [25, 40, 35, 28, 50, 23],
     'Income': [30000, 60000, 58000, 32000, 80000, 25000],
     'Credit Score': [600, 700, 650, 580, 720, 550],
@@ -78,3 +78,51 @@ y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test,y_pred)
 print(accuracy)
 
+"""
+
+data  = pd.read_csv("train.csv")
+# print(data.head())
+
+# print(data.sample(10))
+
+data = data[['Survived','Pclass','Sex','Age','SibSp','Parch','Fare']]
+
+data['Sex'] = data['Sex'].map({'male' :0,'female':1})
+
+data['Age'].fillna(data['Age'].mean(),inplace=True)
+
+# print(data.isnull().sum())
+
+x = data.drop(['Survived'],axis=1)
+y= data['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(x_train,y_train)
+
+y_pred = model.predict(x_test)
+# y_prob = model.predict_proba(x_test)
+
+# print("y_pred :",y_pred)    
+# print("y_prob :",y_prob)
+
+print("accuracy :",accuracy_score(y_test,y_pred))
+
+# confusion matrix : 
+
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+"""
+# confusion  matrix :                     predicted positive     predicted negative
+actual positive                             TP                     FN
+actual negative                             FP                     TN
+
+"""
+print("precision : ",precision_score(y_test,y_pred))
+print("recall : ",recall_score(y_test,y_pred))
+print("f1 score : ",f1_score(y_test,y_pred))
+print("accuracy : ",accuracy_score(y_test,y_pred))
+print("specificity : ",recall_score(y_test,y_pred,pos_label=0))
+
+# knn , decison tree ,  random forest , SVM : 
